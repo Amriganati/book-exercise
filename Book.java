623fb10064a1c7b9cc22c0b0e9f3bf8cf88fe0de
Amriganati <30274109+Amriganati@users.noreply.github.com>
2019-02-04 16:03:16 -0500

    .92
    added course text boolean.

--- Book.java
+++ Book.java
@@ -14,47 +14,65 @@ class Book
     private int pages;
     private String refNumber;
     private int borrowed;
-    private Boolean courseText;
+    private boolean courseText;
     
     /**
-     * Set the author and title fields when this object
-     * is constructed.
+     * Set the author, title,and page count fields when this object
+     * is constructed. as well as instanciate a 0 value for refNumber. 
      */
     public Book(String bookAuthor, String bookTitle, int bookPages, 
-     String refNumber)
+     String refNumber, boolean isCourseText)
     {
         author = bookAuthor;
         title = bookTitle;
         pages = bookPages;
         refNumber = "";
+        courseText = isCourseText;
        
     }
 
-    // Add the methods here ...
+    /**
+     * gets the author of the book.
+     */
     public String getAuthor()   
     { 
         return author;
     }
-    
+    /**
+     * Gets the title of the book.
+     */
     public String getTitle()
     {
         return title;
 
     }
     
+    /**
+     * gets the number of pages in the book.
+     */
     public int getPages()
     {
         return pages;
     }
     
+    /**
+     * Returns number of times book has been borrowed.
+     */
     public int getBorrowed()
     {
         return borrowed;
     }
     
+    /**
+     * Adds one to number of times book has been borrowed.
+     */
     public void borrow()
     {
         borrowed += 1;
+    }
+    
+    public boolean isCourseText(){
+     return courseText;
     }
     
     /** 
@@ -84,8 +102,8 @@ class Book
         System.out.println("Pages: " + pages);
         System.out.println("Checked out?: " + borrowed);
         /**
-         * would only work if a function to record books being checked out
-         * was programmed.
+         * would only work if a function to record books being checked
+         * back in was programmed.
         *if(borrowed() > 0)
         *{
         *    System.out.println("This book is currently checked out!")
@@ -99,6 +117,10 @@ class Book
             System.out.println("Reference Number: N/A");
         }
     }
+    /**
+     * Checks to see if the reference number for book is valid format.
+     * If not it yells at you.
+     */
     public void setRefNumber(String ref)
     {
         if(ref.length() >= 3){
